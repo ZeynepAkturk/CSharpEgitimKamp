c6ffd72f2672e70e69cd59764a1c02d47213e542
Zeynep AKTÜRK <zeynepakturk.maibordo@gmail.com>
2024-10-11 16:56:43 +0300

    Proje dosyası ekle.

---
+++ 03_MakingDecision/Program.cs
@@ -0,0 +1,80 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+#region If Else
+
+//örnek 1 : If-Else kullanımı
+//Console.WriteLine("Lütfen şifreyi giriniz:");
+//string password;
+//password = Console.ReadLine();
+//if(password=="abcd")
+//{
+//    Console.WriteLine("Şifre doğru.");
+//}
+//else
+//{
+//    Console.WriteLine("şifre yanlış");
+//}
+
+//örnek 2: If-Else kullanımı
+
+//string capital, country;
+//Console.Write("Başkenti giriniz :");
+//capital = Console.ReadLine();
+
+//Console.WriteLine("Ülkeyi giriniz:");
+//country = Console.ReadLine();
+
+//if(capital=="Ankara" & country=="Türkiye")
+//{
+//    Console.WriteLine("veriler doğrulandı.");
+//}
+//else
+//{
+//    Console.WriteLine("hatalı bilgi");
+//}
+
+//örnek 3: If-Else kullanımı
+//int exam1, exam2, exam3, average;
+//string result="geçerli bir değer değil";
+//Console.WriteLine("Sınav1:");
+//exam1 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Sınav2:");
+//exam2 = int.Parse(Console.ReadLine());
+//Console.WriteLine("Sınav3:");
+//exam3 = int.Parse(Console.ReadLine());
+
+//average = (exam1 + exam2 + exam3) / 3;
+//Console.WriteLine("Sınavların ortalaması: "+ average);
+
+//if(average>0 &average<=50)
+//{
+//    result = "Sonuç vasat";
+//}
+//if(average>50 & average<=70)
+//{
+//    result = "Sonuç iyi";
+//}
+//if(average>70 & average<=84)
+//{
+//    result = "Sonuç iyi";
+//}
+//if (average > 84 )
+//{
+//    result = "Sonuç çok iyi";
+//}
+
+//Console.WriteLine(result);
+
+
+#endregion
+
+#region Mod İşlemleri
+
+int number;
+number = 26;
+int result = number % 5;
+Console.WriteLine(result);
+
+#endregion
---
+++ CSharpTemeli/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Vira Bismillah
+
+
+namespace CSharpTemeli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region YazdırmaKomutları
+            //Console.Write( "Merhaba Dünya");
+            //Console.Read(); // konsol ekranını enter dışına basana kadar tut.
+
+            #endregion
+
+            #region Değişkenler
+            // String Değişkenler
+            string customerName; //camelCase
+            customerName = "Zeynep";
+            Console.WriteLine(customerName);
+            string customerSurname, customerPhone, customerEmail, district, city;
+            customerSurname = "Aktürk";
+            customerPhone = "+90 500 349 55 61";
+            customerEmail = "deneme@gmail.com";
+            district = "İlkadım";
+            city = "Samsun";
+            Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+
+            //Int Değişkenler
+            int number = 12;
+            Console.WriteLine(number);
+
+            //Double Değişkenler
+            double numberDouble = 4.56;
+            Console.WriteLine(numberDouble); //double değişken . kullanılarak tanımlanırken konsol ekranında çıktısı virgül olur.
+            Console.OutputEncoding = System.Text.Encoding.UTF8; //özel karakterleri konsolda görebilmek için kullandık.
+            Console.WriteLine("₺");
+            double orangeGram = 3300;
+            double orangePrice = 10;
+            double orangeTotalPrice = orangeGram * orangePrice;
+            Console.WriteLine("Orange price : " + orangeTotalPrice + " ₺");
+
+
+            //Char Değişkenler
+            char symbol = 'a';
+            Console.WriteLine(symbol);
+
+            //Klavyeden Veri Girişleri String Değişkenler
+            Console.WriteLine("CHARP HAVA YOLLLARI YOLCU BİLGİSİ");
+            //ctr+f customer --> passenger
+            string passengerName, passengerSurname;
+            Console.WriteLine("Yolcu Adı: ");
+            passengerName = Console.ReadLine(); //kalvyeden veri girişi
+            Console.WriteLine(passengerName);
+
+            //Klavyeden Tam Sayı Girişleri ve Dönüşümler
+            int shoePrice, computerPrice, chairPrice, tvPrice;
+            shoePrice = 1000;
+            computerPrice = 20000;
+            chairPrice = 500;
+            tvPrice = 12000;
+            int shoesCount, computerCount, tvCount;
+            Console.WriteLine("Lütfen aldığınız ayakkabı sayısını giriniz.");
+            shoesCount = int.Parse(Console.ReadLine()); //Parse() : dönüşüm işlemi için kullanılır.
+            Console.WriteLine("Alınan ayakkabıların ücreti: " + shoesCount * shoePrice);
+
+            //Klavyeden ondalıklı sayı işlemleri
+            Console.WriteLine("Lütfen sınav notunuzu giriniz.");
+            double examNote = double.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+
+
+
+
+            
+
+            #endregion 
+
+
+        }
+    }
+}
