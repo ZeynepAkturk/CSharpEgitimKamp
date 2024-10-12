da229bf0f56bf8de23ae2470e98e7100a263e3cf
Zeynep AKTÜRK <zeynepakturk.maibordo@gmail.com>
2024-10-12 17:39:20 +0300

    Döngüler konusu işlendi.

--- 04_Loops/Program.cs
+++ 04_Loops/Program.cs
@@ -1,2 +1,76 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+#region For Döngüsü
+
+//Döngüler
+// For(x;y;z)
+//x: başlangıç, y:bitiş, z:artış-azalış
+using System.Net.NetworkInformation;
+
+//int i;
+//for ( i=1; i<=5; i++)
+//{
+//    Console.WriteLine("CSharp Kampı");
+//}
+
+//for (int j= 0; j <= 20; j+=3)
+//{
+//    Console.WriteLine(j);
+//}
+#endregion
+
+#region For Döngüsünün Karar Yapılarıyla Kullanılması
+
+//Örnek 1: 0 dan 100'e kadar 5'e tam bölünen sayılar
+
+//for(int i=0; i<=100; i++)
+//{
+//    if(i%5==0)
+//    {
+//        Console.WriteLine(i);
+//    }
+//}
+
+//Örnek 2: 1 den 10'a kadar olan sayıların toplamı
+
+//int totalValue = 0;
+//for(int j=0; j<=10; j++)
+//{
+//    totalValue += j;
+//}
+//Console.WriteLine(totalValue);
+
+#endregion
+
+#region While Döngüsü
+//While döngüsü, şart sağlandığı müddetçe devam eder.
+
+//int i = 0;
+//while(i<=10)
+//{
+//    i++;
+//    Console.WriteLine(i);
+//}
+
+//Örnek 1: Klavyeden girilen 3 basamaklı sayının basamakları toplamını hesaplayan kodu yazınız.
+//456
+//Console.WriteLine("Sayıyı giriniz.");
+//int number = int.Parse(Console.ReadLine());
+//int ones, tens, hundreds;
+//int sum;
+//hundreds = number / 100;
+//ones = number % 10;
+//tens = (number%100)/10;
+
+//Console.WriteLine(hundreds);
+//Console.WriteLine(tens);
+//Console.WriteLine(ones);
+
+//sum = hundreds + tens + ones;
+//Console.WriteLine(sum);
+
+
+#endregion
+
+
